64947fafe878e26920c96f34bf2da90e98ddfe29
NethulTJ <nethultrevor@gmail.com>
2026-01-08 23:14:45 +0530

    edited app.jsx tobe compatible with search and property details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import propertiesData from "./assets/data/properties.json";
 import Search from "./components/Search";
+import PropertyDetails from "./components/PropertyDetails";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Estate Agent Web App</h1>
+
+      <Routes>
+        <Route
+          path="/"
+          element={<Search properties={propertiesData.properties} />}
+        />
+
+        <Route
+          path="/property/:id"
+          element={<PropertyDetails properties={propertiesData.properties} />}
+        />
+      </Routes>
     </div>
   );
 }
